6a34c6078733ced4c9dbe35580a402ae13e54573
derbyTS <derbysalvacion@gmail.com>
2023-10-01 22:25:01 +0800

    first commit to lazy

---
+++ lua/derby/plugins/lsp/lspconfig.lua
@@ -0,0 +1,185 @@
+return {
+	"neovim/nvim-lspconfig",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		{ "antosha417/nvim-lsp-file-operations", config = true },
+	},
+	config = function()
+		-- import lspconfig plugin
+		local lspconfig = require("lspconfig")
+
+		-- import cmp-nvim-lsp plugin
+		local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+		local keymap = vim.keymap -- for conciseness
+
+		local opts = { noremap = true, silent = true }
+		local on_attach = function(client, bufnr)
+			opts.buffer = bufnr
+			keymap.set("n", "gf", "<cmd>Lspsaga lsp_finder<CR>", opts) -- show definition, references
+			-- keymap.set("n", "gDD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts) -- got to declaration
+			vim.keymap.set("n", "gDD", vim.lsp.buf.declaration, opts)
+			vim.keymap.set("n", "gd", function()
+				vim.lsp.buf.definition()
+			end, opts)
+			keymap.set("n", "gD", "<cmd>Lspsaga peek_definition<CR>", opts) -- see definition and make edits in window
+			keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts) -- go to implementation
+			keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts) -- see available code actions
+			keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts) -- smart rename
+			keymap.set("n", "<leader>D", "<cmd>Lspsaga show_line_diagnostics<CR>", opts) -- show  diagnostics for line
+			keymap.set("n", "<leader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts) -- show diagnostics for cursor
+			keymap.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts) -- jump to previous diagnostic in buffer
+			keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts) -- jump to next diagnostic in buffer
+			keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts) -- show documentation for what is under cursor
+			keymap.set("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", opts) -- see outline on right hand side
+		end
+
+		-- used to enable autocompletion (assign to every lsp server config)
+		local capabilities = cmp_nvim_lsp.default_capabilities()
+
+		-- Change the Diagnostic symbols in the sign column (gutter)
+		-- (not in youtube nvim video)
+		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+		for type, icon in pairs(signs) do
+			local hl = "DiagnosticSign" .. type
+			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+		end
+
+
+		lspconfig["ansiblels"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
+
+		lspconfig["clangd"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			settings = {
+				clangd = {
+					format = {
+						style = "file",
+						tabSize = 2,
+					},
+				},
+			},
+		})
+
+		lspconfig["bashls"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
+
+		lspconfig["docker_compose_language_service"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
+
+		local util = require("lspconfig/util")
+		lspconfig["gopls"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			cmd = { "gopls" },
+			filetypes = { "go", "gomod", "gowork", "gotmpl", "gosum" },
+			root_dir = util.root_pattern("go.work", "go.mod", "git"),
+			settings = {
+				gopls = {
+					completeUnimported = true,
+					usePlaceholders = true,
+					analyses = {
+						unusedparamas = true,
+					},
+				},
+			},
+		})
+
+		lspconfig["bashls"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
+
+		lspconfig["jsonls"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
+
+		lspconfig["jdtls"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			cmd = { "jdtls" },
+			filetypes = { "java" },
+			settings = {
+				java = {
+					classpath = "/Library/Java/JavaVirtualMachines/jdk-18.0.1.1.jdk/",
+
+					formatting = {
+						tabSize = 4, -- Adjust this value to your preferred tab size
+					},
+					completeUnimported = true,
+					signatureHelp = { enabled = true }, -- Enable signature help (parameter hints)
+					contentProvider = { preferred = "fernflower" }, -- Preferred decompiler for class files
+					jdt = {
+						completion = {
+							overwrite = true, -- Overwrite existing imports when auto-importing
+						},
+						sources = {
+							organizeImports = {
+								starThreshold = 9999, -- Specify the number of imports for "import *" to trigger sorting
+							},
+						},
+					},
+				},
+				editor = {
+					tabSize = 4,
+				},
+			},
+		})
+
+		lspconfig["marksman"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
+
+		lspconfig["pyright"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
+
+		lspconfig["rust_analyzer"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
+
+		lspconfig["sqlls"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
+
+		lspconfig["yamlls"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
+
+		-- configure lua server (with special settings)
+		lspconfig["lua_ls"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			settings = { -- custom settings for lua
+			Lua = {
+				-- make the language server recognize "vim" global
+				diagnostics = {
+					globals = { "vim" },
+				},
+				workspace = {
+					-- make language server aware of runtime files
+					library = {
+						[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+						[vim.fn.stdpath("config") .. "/lua"] = true,
+					},
+				},
+			},
+		},
+	})
+
+end,
+}
--- lua/derby/plugins/lualine.lua
+++ lua/derby/plugins/lualine.lua
@@ -1,40 +1,47 @@
-local status, lualine = pcall(require, "lualine")
-if not status then
-    return
-end
-
--- get default theme
+return {
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local lualine = require("lualine")
+		local lazy_status = require("lazy.status") -- to configure lazy pending updates count
 local colorscheme = require("lualine.themes.auto")
+		local new_colors = {
+			EED7DC = "#EED7DC",
+			FFCCD1 = "#FFCCD1",
+			FF8B99 = "#FF8B99",
+			FFDA7B = "#FFCCD1",
+			FF3F56 = "#FF3F56",
+		}
 
--- new colors for theme
-local new_colors = {
-  EED7DC = "#EED7DC",
-  FFCCD1 = "#FFCCD1",
-  FF8B99 = "#FF8B99",
-  FFDA7B = "#FFCCD1",
-  FF3F56 = "#FF3F56",
+
+		colorscheme.normal.a.bg = new_colors.EED7DC
+		colorscheme.insert.a.bg = new_colors.FFCCD1
+		colorscheme.visual.a.bg = new_colors.FF8B99
+		colorscheme.command = {
+			a = {
+				gui = "bold",
+				bg = new_colors.FF3F56,
+				fg = new_colors.black, -- black
+			},
+		}
+
+		-- configure lualine with modified theme
+		lualine.setup({
+			options = {
+				theme = colorscheme,
+			},
+			sections = {
+				lualine_x = {
+					{
+						lazy_status.updates,
+						cond = lazy_status.has_updates,
+						color = { fg = "#ff9e64" },
+					},
+					{ "encoding" },
+					{ "fileformat" },
+					{ "filetype" },
+				},
+			},
+		})
+	end,
 }
-
-
-colorscheme.normal.a.bg = new_colors.EED7DC
-colorscheme.insert.a.bg = new_colors.FFCCD1
-colorscheme.visual.a.bg = new_colors.FF8B99
-colorscheme.command = {
-  a = {
-    gui = "bold",
-    bg = new_colors.FF3F56,
-    fg = new_colors.black, -- black
-  },
-}
-
-
-
-
--- configure lualine with modified theme
-lualine.setup({
-  options = {
-       theme = colorscheme,
-  },
-})
-
-
