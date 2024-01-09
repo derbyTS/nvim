8c7fe3f7483cacd113485e008597ffbf85d82b55
derbyTS <derbysalvacion@gmail.com>
2024-01-09 15:59:14 +0800

    Add gv in copy in visual mode so the cursor stay in current location after copying

--- lua/derby/core/keymaps.lua
+++ lua/derby/core/keymaps.lua
@@ -67,8 +67,8 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- Delete and Paste
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+ygv<Esc>]])
+vim.keymap.set("n", "<leader>Y", [["+Ygv<Esc>]])
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 vim.keymap.set("n", "Q", "<nop>")
