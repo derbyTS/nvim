78c85e4e595f0d472d4c99a253066f3fd4c32665
derbyTS <derbysalvacion@gmail.com>
2024-12-09 04:15:22 +0800

    fix the default color scheme

--- lua/derby/lazy.lua
+++ lua/derby/lazy.lua
@@ -16,8 +16,8 @@ require("lazy").setup(
 	{
 		install = {
 			-- colorscheme = { "rose-pine" },
-			-- colorscheme ={"catppuccin/nvim", name = "catppuccin"}
-			colorscheme = { "uloco/bluloco.nvim" },
+			colorscheme = { "catppuccin/nvim", name = "catppuccin" },
+			-- colorscheme = { "uloco/bluloco.nvim" },
 		},
 		checker = {
 			enabled = true,
